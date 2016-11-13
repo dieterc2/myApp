344c8626aa27192a83b42a058a0749f61d044437
chris dieter <dieterc2@miamioh.edu>
2016-11-13 16:31:37 -0500

    file structure

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>hello world</h1>
+        <p>hows it going</p>
+      </div>
+
+    );
+  }
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,29 @@
-var webpack = require('webpack');
-var path = require('path');
-var nodeModulesPath = path.resolve(__dirname, 'node_modules');
-var buildPath = path.resolve(__dirname, 'public', 'build');
-var mainPath = path.resolve(__dirname, 'app', 'main.js');
-
 var config = {
-    devtool: 'eval',
-    entry: [
-	'webpack/hot/dev-server',
-	'webpack-dev-server/client?http://localhost:8080',
-	mainPath
-    ],
-    output: {
-	path: buildPath,
-	filename: 'bundle.js',
+   entry: './main.js',
 
-	publicPath: '/build/'
-    },
-    module: {
+   output: {
+      path:'./',
+      filename: 'index.js',
+   },
 
-	loaders: [
-	    {
-		test: /\.js$/,
-		loader: 'babel',
-		exclude: [nodeModulesPath]
-	    },
-	    {
-		test: /\.css$/,
-		loader: 'style!css'
-	    }
-	]
-    },
+   devServer: {
+      inline: true,
+      port: 8080
+   },
 
-    plugins: [new Webpack.HotModuleReplacementPlugin()]
-};
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
 
 module.exports = config;
