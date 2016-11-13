8f38a1d7b4f30189973ba1351f69312ff9854524
chris dieter <dieterc2@miamioh.edu>
2016-11-13 16:51:29 -0500

    adding materialize

--- App.jsx
+++ App.jsx
@@ -4,12 +4,39 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>hello world</h1>
-        <p>hows it going</p>
+        <Header/>
+        <Content/>
       </div>
-
     );
   }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Runt</a>
+            <ul>
+              <li>ajd</li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+class Content extends React.Component{
+    render() {
+      return (
+        <div>
+          <h2>Contentd</h2>
+          <p>here we go</p>
+        </div>
+      );
+    }
 }
 
 export default App;
