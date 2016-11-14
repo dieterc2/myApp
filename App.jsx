4f6bb2ede2f30f899be524de929b9d54b6ccc190
chris dieter <dieterc2@miamioh.edu>
2016-11-14 16:57:08 -0500

    adding react materialize and react router

--- App.jsx
+++ App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Button, Icon, Navbar, NavItem, Slider, Slide, Col, CardPanel, Row} from 'react-materialize';
 
 class App extends React.Component {
   render() {
@@ -15,14 +16,10 @@ class Header extends React.Component {
   render() {
     return (
       <div>
-        <nav>
-          <div class="nav-wrapper">
-            <a href="#" class="brand-logo">Runt</a>
-            <ul>
-              <li>ajd</li>
-            </ul>
-          </div>
-        </nav>
+        <Navbar brand='TuckFrump' right>
+          <NavItem href='get-started.html'>Getting started</NavItem>
+          <NavItem href='components.html'>Components</NavItem>
+        </Navbar>
       </div>
     );
   }
@@ -32,8 +29,35 @@ class Content extends React.Component{
     render() {
       return (
         <div>
-          <h2>Contentd</h2>
-          <p>here we go</p>
+          <Slider>
+            <Slide
+              src="http://lorempixel.com/580/250/nature/1"
+              title="This is our big Tagline!">
+              Here's our small slogan.
+            </Slide>
+            <Slide
+              src="http://lorempixel.com/580/250/nature/2"
+              title="Left aligned Caption"
+              placement="left">
+              Here's our small slogan.
+            </Slide>
+            <Slide
+              src="http://lorempixel.com/580/250/nature/3"
+              title="Right aligned Caption"
+              placement="right">
+              Here's our small slogan.
+            </Slide>
+          </Slider>
+          <Row>
+            <Col s={12} m={5}>
+                <CardPanel className="teal lighten-4 black-text">
+                    I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                </CardPanel>
+            </Col>
+            <Col s={12} m={7}>
+                For a simpler card with less markup, try using a card panel which just has padding and a shadow effect
+            </Col>
+          </Row>
         </div>
       );
     }
