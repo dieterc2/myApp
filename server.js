a5cf0c19b852b66ccf300251adc8920eca7ac49d
Chris Dieter <c.dieter@lilly.com>
2016-11-22 18:31:25 -0600

    working on express

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 import express from 'express';
 import webpack from 'webpack';
+import path from 'path';
 
 const config = require('./webpack.config.js');
 
+const port = 3000;
+
 const app = express();
 const compiler = webpack(config);
+
+app.use(express.static(path.join(__dirname, '/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
